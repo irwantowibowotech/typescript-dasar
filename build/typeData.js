86633f09a906b8c762ee3660ec91457cf34a6a00
irwantowibowotech <66348834+irwantowibowotech@users.noreply.github.com>
2022-08-07 00:09:48 +0700

    memahami tipe data number dan boolean (#2)
    Co-authored-by: irwantojm <iwibowo@juraganmaterial.id>

--- build/typeData.js
+++ build/typeData.js
@@ -1,4 +1,13 @@
 "use strict";
+// Tipe data string
 let nama = "Irwanto";
 nama = "Irwanto Wibowo";
 console.log("Nama :", nama);
+// Tipe data number
+let nomor = 20;
+nomor = 23;
+console.log(nomor);
+// Tipe data boolean
+let isMarriage = true;
+isMarriage = false;
+console.log(isMarriage);
