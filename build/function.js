af87cdd9ed850e9f61638513ad70f95bc129e3c2
irwantowibowotech <66348834+irwantowibowotech@users.noreply.github.com>
2022-08-09 06:26:48 +0700

    memahami return function (#7)
    Co-authored-by: irwantojm <iwibowo@juraganmaterial.id>

---
+++ build/function.js
@@ -0,0 +1,14 @@
+"use strict";
+// tipe data balikan pada function
+function getName() {
+    return "Hello, nama saya Irwanto";
+}
+console.log(getName());
+function getAge() {
+    return 30;
+}
+console.log(getAge());
+function printName() {
+    console.log("Print name");
+}
+printName();
---
+++ src/function.ts
@@ -0,0 +1,18 @@
+// tipe data balikan pada function
+function getName(): string {
+    return "Hello, nama saya Irwanto";
+}
+
+console.log(getName());
+
+function getAge(): number {
+    return 30;
+}
+
+console.log(getAge());
+
+function printName(): void {
+    console.log("Print name");
+}
+
+printName();
