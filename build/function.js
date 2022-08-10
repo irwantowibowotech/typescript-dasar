728e8c572b98771f76f1dbe6bf3a89f8de8baa98
irwantojm <iwibowo@juraganmaterial.id>
2022-08-11 06:43:09 +0700

    memahami default parameter di fungsi

--- build/function.js
+++ build/function.js
@@ -23,3 +23,12 @@ function tambah(angka1, angka2) {
     return angka1 + angka2;
 }
 console.log(tambah(40, 20));
+const Add = (val1, val2) => {
+    return val1 + val2;
+};
+// Default parameter
+const getNama = (firstName, lastName = "Wibowo") => {
+    return firstName + " " + lastName;
+};
+console.log(getNama("Irwanto")); // Irwanto Wibowo
+console.log(getNama("Irwanto", "Mantabb")); // Irwanto Mantabb
