b49cfb671a58b5bc0683963a256a7f11d0b0fea0
irwantowibowotech <66348834+irwantowibowotech@users.noreply.github.com>
2022-08-14 12:09:16 +0700

    memahami pewarisan di typescript (#13)

--- build/class.js
+++ build/class.js
@@ -6,8 +6,40 @@ class User {
         this.name = name;
         this.name = name;
     }
+    setName(value) {
+        this.name = value;
+    }
+    getName() {
+        return this.name;
+    }
 }
 exports.User = User;
 let user = new User("Irwanto");
 console.log(user);
 console.log(user.name);
+/*
+* Hak akses pada class
+*
+* public : bisa diakses di semua class/dari luar class
+* protected: hanya bisa diakses dari class tersebut, dan class turunannya
+* private: hanya bisa diakses dari class itu sendiri
+ */
+// Inheritance
+class Admin extends User {
+    constructor() {
+        super(...arguments);
+        this.read = true;
+        this.write = true;
+    }
+    getRole() {
+        return {
+            read: this.read,
+            write: this.write,
+        };
+    }
+}
+let admin = new Admin("Admin");
+console.log(admin.getName()); // Admin
+console.log(admin.getRole()); // { read: true, write: true }
+admin.setName("Mantabb");
+console.log(admin.getName()); // Mantabb
--- src/class.ts
+++ src/class.ts
@@ -2,8 +2,43 @@ export class User {
     constructor(public name: string) {
         this.name = name;
     }
+
+    setName(value: string) {
+        this.name = value;
+    }
+
+    getName(): string {
+        return this.name;
+    }
 }
 
 let user = new User("Irwanto");
 console.log(user);
 console.log(user.name);
+
+/*
+* Hak akses pada class
+*
+* public : bisa diakses di semua class/dari luar class
+* protected: hanya bisa diakses dari class tersebut, dan class turunannya
+* private: hanya bisa diakses dari class itu sendiri
+ */
+
+// Inheritance
+class Admin extends User {
+    read: boolean = true;
+    write: boolean = true;
+
+    getRole(): {read: boolean, write: boolean} {
+        return {
+            read: this.read,
+            write: this.write,
+        }
+    }
+}
+
+let admin = new Admin("Admin");
+console.log(admin.getName()); // Admin
+console.log(admin.getRole()); // { read: true, write: true }
+admin.setName("Mantabb");
+console.log(admin.getName()); // Mantabb
